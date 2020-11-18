d8c0c67878398c4fd67e48be5e511b13bb60f8e1
Antonio Niño Díaz <antonio_nd@outlook.com>
2020-11-18 02:31:12 +0000

    build: Allow more image extensions for grit
    Support PNG, GIF and JPG.

--- libgbaline/cmake/grit.cmake
+++ libgbaline/cmake/grit.cmake
@@ -9,46 +9,50 @@ set(GRIT_PATH "$ENV{DEVKITPRO}/tools/bin/grit"
 function(add_grit_files source_directory destination_target)
 
     file(GLOB PNG_FILES "${source_directory}/*.png")
+    file(GLOB GIF_FILES "${source_directory}/*.gif")
+    file(GLOB JPG_FILES "${source_directory}/*.jpg")
+
+    set(GFX_FILES ${PNG_FILES} ${GIF_FILES} ${JPG_FILES})
 
     file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${source_directory}")
 
-    foreach(PNG_FILE ${PNG_FILES})
+    foreach(GFX_FILE ${GFX_FILES})
 
         # First, get full path of the file without extension
 
-        get_filename_component(PNG_DIR ${PNG_FILE} DIRECTORY)
-        get_filename_component(PNG_NAME ${PNG_FILE} NAME_WLE)
+        get_filename_component(GFX_DIR ${GFX_FILE} DIRECTORY)
+        get_filename_component(GFX_NAME ${GFX_FILE} NAME_WLE)
 
-        set(PATH_NO_EXT "${PNG_DIR}/${PNG_NAME}")
+        set(PATH_NO_EXT "${GFX_DIR}/${GFX_NAME}")
 
-        # Now, check that there is a .grit file along the .png file
+        # Now, check that there is a .grit file along the graphics file
 
         set(GRIT_FILE "${PATH_NO_EXT}.grit")
 
         if(NOT EXISTS ${GRIT_FILE})
-            message(FATAL_ERROR "PNG file without GRIT file: ${PNG_FILE}")
+            message(FATAL_ERROR "GFX file without GRIT file: ${GFX_FILE}")
         endif()
 
         # Generate the output file names
 
         set(OUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/${source_directory}")
-        set(OUT_C_FILE "${OUT_DIR}/${PNG_NAME}.c")
-        set(OUT_H_FILE "${OUT_DIR}/${PNG_NAME}.h")
+        set(OUT_C_FILE "${OUT_DIR}/${GFX_NAME}.c")
+        set(OUT_H_FILE "${OUT_DIR}/${GFX_NAME}.h")
 
         set(OUT_FILES ${OUT_C_FILE} ${OUT_H_FILE})
 
-        # Create rule that depends on the PNG and GRIT file
+        # Create rule that depends on the GFX and GRIT file
 
         add_custom_command(
             OUTPUT ${OUT_FILES}
-            COMMAND ${GRIT_PATH} ${PNG_FILE} -ftc -o ${PNG_NAME}
-            DEPENDS ${GRIT_FILE} ${PNG_FILE}
+            COMMAND ${GRIT_PATH} ${GFX_FILE} -ftc -o ${GFX_NAME}
+            DEPENDS ${GRIT_FILE} ${GFX_FILE}
             WORKING_DIRECTORY ${OUT_DIR}
         )
 
         # Add output source files to the target
 
-        message(VERBOSE "${destination_target}: Adding ${PNG_NAME}.png")
+        message(VERBOSE "${destination_target}: Adding ${GFX_FILE}")
 
         target_sources(${destination_target} PRIVATE ${OUT_FILES})
         target_include_directories(${destination_target} PRIVATE ${OUT_DIR})
