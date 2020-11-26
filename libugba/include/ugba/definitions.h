152742bed703dd9c61869ce048b3d50fabd07853
Antonio Niño Díaz <antonio_nd@outlook.com>
2020-11-26 02:07:59 +0000

    library: Add EWRAM/IWRAM and ARM/THUMB macros

--- libugba/include/ugba/definitions.h
+++ libugba/include/ugba/definitions.h
@@ -32,4 +32,22 @@
 # endif
 #endif
 
+#ifdef __GBA__
+# define ARM_CODE   __attribute__((target("arm")))
+# define THUMB_CODE __attribute__((target("thumb")))
+# define IWRAM_DATA __attribute__((section(".iwram")))
+# define EWRAM_DATA __attribute__((section(".ewram")))
+# define EWRAM_BSS  __attribute__((section(".sbss")))
+# define IWRAM_CODE __attribute__((section(".iwram"), long_call))
+# define EWRAM_CODE __attribute__((section(".ewram"), long_call))
+#else
+# define ARM_CODE
+# define THUMB_CODE
+# define IWRAM_DATA
+# define EWRAM_DATA
+# define EWRAM_BSS
+# define IWRAM_CODE
+# define EWRAM_CODE
+#endif
+
 #endif // DEFINITIONS_H__
