11520020af46431f2c72b5fcd89a1b3325adb078
Antonio Niño Díaz <antonio_nd@outlook.com>
2020-11-30 21:36:50 +0000

    examples: Add comments to some Lua scripts

--- examples/timers/timer_interrupt/test-sdl2.lua
+++ examples/timers/timer_interrupt/test-sdl2.lua
@@ -1,6 +1,9 @@
--- Test that runs a program for 70 frames, takes a screenshot and exits.
+-- Test that runs a program for 70 frames, takes a screenshot, presses A to
+-- show the results screen, takes a screenshot and exits.
 
 run_frames_and_pause(70)
+-- This screenshot is used as reference when the test fails, it isn't used for
+-- the test checks.
 screenshot("timer-values.png")
 keys_hold("A")
 
