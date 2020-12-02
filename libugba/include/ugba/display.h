a3c65cbd5b86f2302860c65f88bb45b3c8034921
Antonio Niño Díaz <antonio_nd@outlook.com>
2020-12-02 02:28:34 +0000

    library: Add display control interfaces

--- libugba/include/ugba/display.h
+++ libugba/include/ugba/display.h
@@ -10,4 +10,19 @@
 // Set background display mode without affecting the other flags of REG_DISPCNT.
 EXPORT_API void DISPLAY_ModeSet(int mode);
 
+// Enable or disable screen layers.
+EXPORT_API void DISPLAY_LayersEnable(int bg0, int bg1, int bg2, int bg3, int obj);
+
+// Enable or disable windows.
+EXPORT_API void DISPLAY_WindowsEnable(int win0, int win1, int obj);
+
+// Enable or disable the HBL Interval Free mode.
+EXPORT_API void DISPLAY_HBLIntervalFreeEnable(int enable);
+
+// Enable or disable forced blank.
+EXPORT_API void DISPLAY_ForcedBlankEnable(int enable);
+
+// Enable 1D mapping or disable it (and use 2D mapping).
+EXPORT_API void DISPLAY_Object1DMappingEnable(int enable);
+
 #endif // DISPLAY_H__
--- libugba/source/display.c
+++ libugba/source/display.c
@@ -9,3 +9,62 @@ void DISPLAY_ModeSet(int mode)
     REG_DISPCNT &= ~DISPCNT_BG_MODE_MASK;
     REG_DISPCNT |= DISPCNT_BG_MODE(mode);
 }
+
+void DISPLAY_LayersEnable(int bg0, int bg1, int bg2, int bg3, int obj)
+{
+    uint16_t mask = DISPCNT_BG0_ENABLE | DISPCNT_BG1_ENABLE |
+                    DISPCNT_BG2_ENABLE | DISPCNT_BG3_ENABLE |
+                    DISPCNT_OBJ_ENABLE;
+
+    REG_DISPCNT &= ~mask;
+
+    if (bg0)
+        REG_DISPCNT |= DISPCNT_BG0_ENABLE;
+    if (bg1)
+        REG_DISPCNT |= DISPCNT_BG1_ENABLE;
+    if (bg2)
+        REG_DISPCNT |= DISPCNT_BG2_ENABLE;
+    if (bg3)
+        REG_DISPCNT |= DISPCNT_BG3_ENABLE;
+    if (obj)
+        REG_DISPCNT |= DISPCNT_OBJ_ENABLE;
+}
+
+void DISPLAY_WindowsEnable(int win0, int win1, int obj)
+{
+    uint16_t mask = DISPCNT_WIN0_ENABLE | DISPCNT_WIN1_ENABLE |
+                    DISPCNT_OBJ_WIN_ENABLE;
+
+    REG_DISPCNT &= ~mask;
+
+    if (win0)
+        REG_DISPCNT |= DISPCNT_WIN0_ENABLE;
+    if (win1)
+        REG_DISPCNT |= DISPCNT_WIN1_ENABLE;
+    if (obj)
+        REG_DISPCNT |= DISPCNT_OBJ_WIN_ENABLE;
+}
+
+void DISPLAY_HBLIntervalFreeEnable(int enable)
+{
+    if (enable)
+        REG_DISPCNT |= DISPCNT_HBL_INTERVAL_FREE;
+    else
+        REG_DISPCNT &= ~DISPCNT_HBL_INTERVAL_FREE;
+}
+
+void DISPLAY_ForcedBlankEnable(int enable)
+{
+    if (enable)
+        REG_DISPCNT |= DISPCNT_FORCED_BLANK;
+    else
+        REG_DISPCNT &= ~DISPCNT_FORCED_BLANK;
+}
+
+void DISPLAY_Object1DMappingEnable(int enable)
+{
+    if (enable)
+        REG_DISPCNT |= DISPCNT_OBJ_1D_MAPPING;
+    else
+        REG_DISPCNT &= ~DISPCNT_OBJ_1D_MAPPING;
+}
