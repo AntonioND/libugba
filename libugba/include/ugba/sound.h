f5a9300a05d824e07796b524c288ae9441972ddd
Antonio Niño Díaz <antonio_nd@outlook.com>
2021-01-02 20:24:42 +0100

    library: Add comments to sound functions

--- libugba/include/ugba/sound.h
+++ libugba/include/ugba/sound.h
@@ -7,24 +7,29 @@
 
 #include "definitions.h"
 
-EXPORT_API
-void SOUND_MasterEnable(int enable);
+// Enable or disable sound hardware. It must be enabled before doing anything
+// else with sound hardware.
+EXPORT_API void SOUND_MasterEnable(int enable);
 
 // Set volume of DMA channels. Valid values are 100 and 50. Any other value will
 // be treated as 50%.
 EXPORT_API void SOUND_DMA_Volume(int dma_a_max, int dma_b_max);
 
+// Specify which DMA channels are heard from which speakers.
 EXPORT_API
 void SOUND_DMA_Pan(int dma_a_left, int dma_a_right,
                    int dma_b_left, int dma_b_right);
 
-EXPORT_API
-void SOUND_DMA_Stream_A(const void *source);
+// Set buffer that will be streamed to DMA channel A. The buffer needs to be
+// aligned to 32 bits.
+EXPORT_API void SOUND_DMA_Stream_A(const void *source);
 
-EXPORT_API
-void SOUND_DMA_Stream_B(const void *source);
+// Set buffer that will be streamed to DMA channel B. The buffer needs to be
+// aligned to 32 bits.
+EXPORT_API void SOUND_DMA_Stream_B(const void *source);
 
-EXPORT_API
-void SOUND_DMA_TimerSetup(int dma_a_timer, int dma_b_timer);
+// Specify which timer is assigned to each one of the DMA channels. Only timers
+// 0 and 1 are valid.
+EXPORT_API void SOUND_DMA_TimerSetup(int dma_a_timer, int dma_b_timer);
 
 #endif // SOUND_H__
