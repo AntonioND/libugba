3919c372c3b2bc8b92289a0caa56beb3dcb3afb5
Antonio Niño Díaz <antonio_nd@outlook.com>
2021-03-09 01:10:38 +0000

    examples: cmake: Define helper for test with 3 screenshots
    There must be some generic way to do this, but I'd rather find it some
    other day.

--- examples/cmake/example.cmake
+++ examples/cmake/example.cmake
@@ -78,6 +78,9 @@ function(define_example)
 
 endfunction()
 
+# TODO: There must be some way to make a generic function for any number of
+# screenshots.
+
 function(unittest_screenshot)
 
     # Get name of the folder we are in
@@ -202,6 +205,90 @@ function(unittest_two_screenshots)
                         -DCMD1=${CMD1}
                         -DCMD2=${CMD2}
                         -DCMD3=${CMD3}
+                        -P ${CMAKE_SOURCE_DIR}/examples/cmake/runcommands.cmake
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        )
+    endif()
+
+endfunction()
+
+function(unittest_three_screenshots)
+
+    # Get name of the folder we are in
+    get_filename_component(EXECUTABLE_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+
+    set(TEST_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/test-sdl2.lua")
+    if(NOT EXISTS ${TEST_SCRIPT})
+        set(TEST_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/test.lua")
+    endif()
+
+    set(REF_1_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-1-sdl2.png")
+    if(NOT EXISTS ${REF_1_PNG})
+        set(REF_1_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-1.png")
+    endif()
+
+    set(REF_2_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-2-sdl2.png")
+    if(NOT EXISTS ${REF_2_PNG})
+        set(REF_2_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-2.png")
+    endif()
+
+    set(REF_3_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-3-sdl2.png")
+    if(NOT EXISTS ${REF_3_PNG})
+        set(REF_3_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-3.png")
+    endif()
+
+    set(CMD1 "$<TARGET_FILE:${EXECUTABLE_NAME}> --lua ${TEST_SCRIPT}")
+    set(CMD2 "$<TARGET_FILE:pngmatch> ${REF_1_PNG} screenshot-1.png")
+    set(CMD3 "$<TARGET_FILE:pngmatch> ${REF_2_PNG} screenshot-2.png")
+    set(CMD4 "$<TARGET_FILE:pngmatch> ${REF_3_PNG} screenshot-3.png")
+
+    add_test(NAME ${EXECUTABLE_NAME}_test
+        COMMAND ${CMAKE_COMMAND}
+                    -DCMD1=${CMD1}
+                    -DCMD2=${CMD2}
+                    -DCMD3=${CMD3}
+                    -DCMD4=${CMD4}
+                    -P ${CMAKE_SOURCE_DIR}/examples/cmake/runcommands.cmake
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    )
+
+    # Emulator test
+    # -------------
+
+    if(BUILD_GBA)
+        set(TEST_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/test-gba.lua")
+        if(NOT EXISTS ${TEST_SCRIPT})
+            set(TEST_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/test.lua")
+        endif()
+
+        set(REF_1_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-1-gba.png")
+        if(NOT EXISTS ${REF_1_PNG})
+            set(REF_1_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-1.png")
+        endif()
+
+        set(REF_2_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-2-gba.png")
+        if(NOT EXISTS ${REF_2_PNG})
+            set(REF_2_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-2.png")
+        endif()
+
+        set(REF_3_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-3-gba.png")
+        if(NOT EXISTS ${REF_3_PNG})
+            set(REF_3_PNG "${CMAKE_CURRENT_SOURCE_DIR}/reference-3.png")
+        endif()
+
+        set(GBA_ROM "${CMAKE_CURRENT_SOURCE_DIR}/${EXECUTABLE_NAME}.gba")
+
+        set(CMD1 "$<TARGET_FILE:giibiiadvance> --lua ${TEST_SCRIPT} ${GBA_ROM}")
+        set(CMD2 "$<TARGET_FILE:pngmatch> ${REF_1_PNG} screenshot-1.png")
+        set(CMD3 "$<TARGET_FILE:pngmatch> ${REF_2_PNG} screenshot-2.png")
+        set(CMD4 "$<TARGET_FILE:pngmatch> ${REF_3_PNG} screenshot-3.png")
+
+        add_test(NAME ${EXECUTABLE_NAME}_gba_test
+            COMMAND ${CMAKE_COMMAND}
+                        -DCMD1=${CMD1}
+                        -DCMD2=${CMD2}
+                        -DCMD3=${CMD3}
+                        -DCMD4=${CMD4}
                         -P ${CMAKE_SOURCE_DIR}/examples/cmake/runcommands.cmake
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         )
