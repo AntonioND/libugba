24e1b074bce8c987296ada976c5572e5948110d7
Antonio Niño Díaz <antonio_nd@outlook.com>
2021-07-25 23:11:36 +0100

    cmake: Add helpers for GBA builds

---
+++ cmake/toolchain_gba_helpers.cmake
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# Copyright (c) 2021 Antonio Niño Díaz
+
+macro(gba_set_compiler_options elf_target)
+    set(ARGS_C
+        -mthumb -mthumb-interwork
+        -mcpu=arm7tdmi -mtune=arm7tdmi
+        -ffunction-sections -fdata-sections
+    )
+
+    target_compile_options(${elf_target} PRIVATE
+        $<$<COMPILE_LANGUAGE:C>:${ARGS_C}>
+    )
+
+    target_compile_definitions(${elf_target} PRIVATE __GBA__)
+
+    target_link_options(${elf_target} PRIVATE
+        -specs=gba.specs
+        -mthumb -mthumb-interwork
+        -Wl,-Map,${elf_target}.map -Wl,--gc-sections
+    )
+endmacro()
+
+macro(make_gba_rom elf_target rom_name game_title game_code)
+    add_custom_command(
+        OUTPUT ${rom_name}.gba
+        COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:${elf_target}> ${rom_name}.gba
+        COMMAND ${DEVKITPRO_TOOLS}gbafix ${rom_name}.gba -t${game_title} -c${game_code}
+        DEPENDS $<TARGET_FILE:${elf_target}>
+    )
+
+    add_custom_target(${rom_name}_target ALL
+        DEPENDS ${rom_name}.gba
+    )
+endmacro()
