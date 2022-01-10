7ffd05b67731d03b9ae8e9eb1a8fef13c007aac7
Antonio Niño Díaz <antonio_nd@outlook.com>
2022-01-10 23:19:38 +0000

    library: Improve IWRAM/EWRAM definitions

--- include/ugba/definitions.h
+++ include/ugba/definitions.h
@@ -42,11 +42,11 @@
 #ifdef __GBA__
 # define ARM_CODE   __attribute__((target("arm")))
 # define THUMB_CODE __attribute__((target("thumb")))
-# define IWRAM_DATA __attribute__((section(".iwram")))
-# define EWRAM_DATA __attribute__((section(".ewram")))
+# define IWRAM_DATA __attribute__((section(".iwram_data")))
+# define EWRAM_DATA __attribute__((section(".ewram_data")))
 # define EWRAM_BSS  __attribute__((section(".sbss")))
-# define IWRAM_CODE __attribute__((section(".iwram"), long_call))
-# define EWRAM_CODE __attribute__((section(".ewram"), long_call))
+# define IWRAM_CODE __attribute__((section(".iwram_code"), long_call))
+# define EWRAM_CODE __attribute__((section(".ewram_code"), long_call))
 #else
 # define ARM_CODE
 # define THUMB_CODE
